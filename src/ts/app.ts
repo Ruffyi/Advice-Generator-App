0d2cdd871f0577e90630ae63d34d886731bf788a
Kamil <kam.nas21@wp.pl>
2022-02-27 11:26:55 +0100

    Do spinner, and ts coding

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,1 +1,40 @@
-console.log('Hello!');
+(() => {
+	enum EventTypes {
+		CLICK = 'click',
+		CONTENTLOADED = 'DOMContentLoaded',
+	}
+
+	interface IAdvice {
+		id: number;
+		advice: string;
+	}
+
+	const rollDice = document.querySelector('[data-roll]') as HTMLButtonElement;
+	const spinner = document.querySelector('[data-spinner]') as HTMLDivElement;
+	const adviceID = document.querySelector('[data-id]') as HTMLSpanElement;
+	const adviceDescription = document.querySelector(
+		'[data-description]'
+	) as HTMLParagraphElement;
+
+	const renderAdviceData = ({ id, advice }: IAdvice) => {
+		adviceID.textContent = `#${id}`;
+		adviceDescription.textContent = advice;
+	};
+
+	const fetchAdviceData = async () => {
+		try {
+			const response = await fetch('https://api.adviceslip.com/advice');
+			const { slip } = await response.json();
+			spinner.style.display = 'none';
+			renderAdviceData(slip);
+		} catch (err) {
+			if (err instanceof Error) {
+				return err.message;
+			}
+			return err;
+		}
+	};
+
+	rollDice?.addEventListener(EventTypes.CLICK, fetchAdviceData);
+	window?.addEventListener(EventTypes.CONTENTLOADED, fetchAdviceData);
+})();
